908655d5fd12083fa1fd108a6ac1944324d112d7
Brian Jordan <bcjordan@gmail.com>
2011-02-26 18:15:47 -0500

    Now returns static image

--- random.rb
+++ random.rb
@@ -1,11 +1,14 @@
 require 'rubygems'
 require 'sinatra'
+require 'open-uri'
 
 get '/' do
   "Hello from Sinatra on Heroku!"
 end
 
 get '/random.jpg' do
-  "Hello from Sinatra on Heroku!"
+  content_type :jpg
+  file = open('http://farm1.static.flickr.com/197/503637906_eaa3df22f4_o.jpg')
+  file.read
 end
 
