d301460de5861e327263613f16ede9bd6221de6d
Brian Jordan <bcjordan@gmail.com>
2011-02-26 18:51:29 -0500

    (crowd applauds)

--- random.rb
+++ random.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'sinatra'
 require 'open-uri'
+require 'rexml/document'
+require 'pp'
 
 get '/' do
   "Hello from Sinatra on Heroku!"
@@ -8,7 +10,23 @@ end
 
 get '/random.jpg' do
   content_type :jpg
-  file = open('http://farm1.static.flickr.com/197/503637906_eaa3df22f4_o.jpg')
+  file = open("#{get_photo_id}")
   file.read
 end
 
+def get_photo_id
+  url = "http://api.flickr.com/services/feeds/photos_public.gne"
+  file = open(url)
+  xml_data = file.read
+
+  doc = REXML::Document.new(xml_data)
+  photo_urls = []
+
+  doc.elements.each("*/entry/link") do |link_tag|
+    if link_tag.attributes["href"][-1..-1] == 'g'
+      photo_urls << link_tag.attributes["href"]
+    end
+  end
+
+  photo_urls.shuffle[0]
+end
