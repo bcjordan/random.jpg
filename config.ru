b1ae852e313e04c5ff30bd4218c8e182bbf67ae3
Brian Jordan <bcjordan@gmail.com>
2011-02-26 17:55:09 -0500

    Add test script, passenger config

---
+++ config.ru
@@ -0,0 +1,2 @@
+require './random'
+run Sinatra::Application
---
+++ random.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+  "Hello from Sinatra on Heroku!"
+end
+
+get '/random.jpg' do
+  "Hello from Sinatra on Heroku!"
+end
+
